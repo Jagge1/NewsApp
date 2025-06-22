5bb1a01d888db3194290041a89400ba5b9bc537c
Jagge1 <hellgrenjacob@hotmail.com>
2025-06-22 21:27:25 +0200

    Learning how to add new pages

--- news-app/app/news/[id].tsx
+++ news-app/app/news/[id].tsx
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View} from "react-native";
 import React from 'react'
+import { useLocalSearchParams } from "expo-router";
 
 const Details = () => {
-
+    const {id} = useLocalSearchParams();
 
   return (
     <View>
-        <Text>[Id]</Text>
+        <Text>Article details: {id}</Text>
     </View>
   )
 }
