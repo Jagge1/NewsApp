d40529b296cf67c5a58b62e6a0dc0365b75e0a9b
Jagge1 <hellgrenjacob@hotmail.com>
2025-06-24 21:40:37 +0200

    added a stylesheet with random colors for testing

---
+++ news-app/app/components/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native'
+import { View } from 'react-native';
+
+export const Colors = {
+  primary: "#ffffff",
+  secondary: "#E5E7EB",
+  teriary: "#1F2937"
+}
+
+const { primary, secondary, teriary} = Colors;
+
+export const StyledContainer = styled.View`
+  flex: 1;
+`;
