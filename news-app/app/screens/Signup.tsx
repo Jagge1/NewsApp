5a6dcd12834700d03d9235ec054e53005c50401e
Jagge1 <hellgrenjacob@hotmail.com>
2025-06-26 20:15:36 +0200

    Very basic styling added to  sign up page

--- news-app/app/screens/Signup.tsx
+++ news-app/app/screens/Signup.tsx
@@ -1,18 +1,80 @@
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, Pressable } from "react-native";
 
 export default function Signup() {
   return(
-    <View>
-      <Text>Sign up</Text>
+    <View className="px-6">
+      <Text 
+        className="
+          self-center
+          w-60
+          text-center
+          font-gill
+          font-bold
+          text-3xl
+          mt-10
+          bg-blue-600
+          text-white
+          p-3
+          rounded
+          mb-10
+        "
+      >
+        NewsCap
+      </Text>
+
+      <Text className="mb-2">Email</Text>
       <TextInput 
-          placeholder="Name"
-          style={{
-          borderWidth: 1,
-          borderColor: "#ccc",
-          padding: 10,
-          borderRadius: 5,
-          marginBottom: 15
-          }}/>
+        placeholder="Name"
+        className="
+          border
+          border-[#ccc]
+          p-2.5
+          rounded
+          mb-4
+        "
+      />
+
+      <Text className="mb-2">Name</Text>
+      <TextInput 
+        placeholder="Name"
+        className="
+          border
+          border-[#ccc]
+          p-2.5
+          rounded
+          mb-4
+        "
+      />
+
+      <Text className="mb-2">Create username</Text>
+      <TextInput 
+        placeholder="Name"
+        className="
+          border
+          border-[#ccc]
+          p-2.5
+          rounded
+          mb-4
+        "
+      />
+
+      <Text className="mb-2">Password</Text>
+      <TextInput 
+        placeholder="Name"
+        className="
+          border
+          border-[#ccc]
+          p-2.5
+          rounded
+          mb-4
+        "
+
+      />
+      <Pressable className="bg-blue-600 p-3 rounded mt-4">
+        <Text className="text-white text-center font-semibold">Create user</Text>
+      </Pressable>
+
+      
     </View>
   )
 }
