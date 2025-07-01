194554432e27b968aa2a4f29c51aab16057e5f10
Jagge1 <hellgrenjacob@hotmail.com>
2025-07-01 22:10:54 +0200

    removed and renamed tailwind files

--- news-app/app/screens/Welcome.tsx
+++ news-app/app/screens/Welcome.tsx
@@ -1,6 +1,5 @@
 import { View, Text, TextInput, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { LinearGradient} from 'expo-linear-gradient';
 
 export default function Welcome() {
   return(
@@ -11,16 +10,16 @@ export default function Welcome() {
 
       <View>
 
-        <Text className="self-center">
+        <Text>
           Welcome to
         </Text>
 
-        <Text className="text-3xl self-center italic font-semibold">
+        <Text>
           NewsCap
         </Text>
 
-      <Pressable className="bg-blue-600 p-3 rounded-3xl mt-4 w-40 self-center align-middle">
-        <Text className="text-white text-center font-semibold">Get started </Text>
+      <Pressable>
+        <Text>Get started </Text>
       </Pressable>
 
       </View>
