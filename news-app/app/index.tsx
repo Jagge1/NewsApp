bb68b69d6d3f95051b5068c2d6ce12abb2626869
Jagge1 <hellgrenjacob@hotmail.com>
2025-07-03 21:34:37 +0200

    Navigation to Home screen working

--- news-app/app/index.tsx
+++ news-app/app/index.tsx
@@ -1,17 +1,70 @@
-import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
-import Welcome from "./screens/Welcome";
-
-export default function Index() {
-  return (
-    <View style={styles.appContainer}>
-      <Welcome></Welcome>
-    </View>  
-  );
-}
+import { router } from "expo-router";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const styles = StyleSheet.create({
-  appContainer: {
+  container: {
     flex: 1,
+    backgroundColor: "#1E88E5",
   },
-});
+
+  text: {
+    fontFamily: "Gill Sans",
+    color: "white",
+    fontSize: 30
+  },
+
+  logo: {
+    fontStyle: "italic",
+    fontWeight: "600",
+    fontFamily: "Gill Sans",
+    color: "white",
+    fontSize: 80,
+  },
+
+  contentContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 200,
+
+  },
+
+  getStartedBtn: {
+    borderWidth: 2,
+    borderColor: "white",
+    padding: 7,
+    borderRadius: 10,
+    marginTop:10
+  }
+})
+
+export default function Index() {
+  return(
+
+  <View style={styles.container}>
+    <SafeAreaView style={{flex: 1}}>
+
+      <View style={styles.contentContainer}>
+
+        <Text style={styles.text}>
+          Welcome to
+        </Text>
+
+        <Text style={styles.logo}>
+          NewsCap
+        </Text>
+
+      <Button 
+        title="Get started" 
+        color="white"
+        onPress={() => router.push("/screens/HomeScreen")}
+        />
+
+      </View>
+
+    </SafeAreaView>
+  </View>
+  
+  )
+}
--- news-app/app/screens/HomeScreen.tsx
+++ news-app/app/screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 
-export default function HomeScreen() {
+export default function Homescreen() {
   return (
     <View>
       <Text>Home Screen</Text>
