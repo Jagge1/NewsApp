4088cbaa1e50fec39f36af9665caedec9c1cec23
Jagge1 <hellgrenjacob@hotmail.com>
2025-07-07 21:59:52 +0200

    Temporary styling

--- news-app/app/screens/HomeScreen.jsx
+++ news-app/app/screens/HomeScreen.jsx
@@ -1,9 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1E88E5",
+    justifyContent: "center",
+  },
+
+  text: {
+    fontFamily: "Gill Sans",
+    color: "white",
+    fontSize: 30
+  },
+
+  logo: {
+    fontStyle: "italic",
+    fontWeight: "600",
+    fontFamily: "Gill Sans",
+    color: "white",
+    fontSize: 80,
+    textAlign: "center",
+    marginBottom: 100
+  },
+})
 
 export default function Homescreen() {
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.logo}>Home Screen</Text>
   
     </View>
   );
