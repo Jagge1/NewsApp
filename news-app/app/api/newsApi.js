5732c9eb3e56b79e541105630433219308e59f1b
Jagge1 <hellgrenjacob@hotmail.com>
2025-07-08 22:13:47 +0200

    api for testing

--- news-app/app/api/newsApi.js
+++ news-app/app/api/newsApi.js
@@ -1,17 +1,24 @@
-async function getNews() {
+import 'dotenv/config'
+
+export async function getNews() {
 
   try {
-
-    const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=fdc223105c634c1696ebb0cfb3883c02');
+    const apiKey = process.env.API_KEY
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+    const response = await fetch(url);
 
     if (!( response).ok) {
         throw new Error('Http rror')
       }
 
       const data = await response.json()
+      data.articles
       console.log(data)
 
   } catch (error) {
     console.error('Something whent wrong')
+    throw error;
   }
 }
+
+export default { getNews }
